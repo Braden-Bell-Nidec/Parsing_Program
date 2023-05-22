4de684b9305bd1e554a8c03a4dd1a1a7ee1d3b04
Braden Bell <Braden.Bell@mail.nidec.com>
2023-05-22 16:59:54 -0500

    Added README

--- Parsing_Program/Parsing_Program.py
+++ Parsing_Program/Parsing_Program.py
@@ -1,13 +1,3 @@
-'''
-Braden Bell
-05-22-23
-Description:
-This Python script is designed to perform an analysis of job responsibilities within various departments of an organization. 
-The user is first asked to input a file name and an outlier percentage threshold, which assists in identifying responsibilities 
-that occur less frequently, or outliers. 
-'''
-
-#TODO: Add the try-except blocks suggested by GPT-4
 import pandas as pd
 import openpyxl as pyxl
 import os
