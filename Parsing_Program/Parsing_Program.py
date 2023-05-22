642f5c73ab9d9bc6733e81c3a45a0cf422526e18
Braden Bell <Braden.Bell@mail.nidec.com>
2023-05-22 15:00:43 -0500

    a

--- Parsing_Program/Parsing_Program.py
+++ Parsing_Program/Parsing_Program.py
@@ -1,6 +1,6 @@
 '''
 Braden Bell
-05-19-23
+05-22-23
 Description:
 This Python script is designed to perform an analysis of job responsibilities within various departments of an organization. 
 The user is first asked to input a file name and an outlier percentage threshold, which assists in identifying responsibilities 
